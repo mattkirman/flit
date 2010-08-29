cb4da511ade9b075de26592b4629c2446bf38b11
Matt Kirman <matt@mattkirman.com>
2010-08-29 13:06:18 +0100

    It's now possible to delete feature and bugfix branches through flit

--- lib/flit/commands/delete.rb
+++ lib/flit/commands/delete.rb
@@ -1,7 +1,7 @@
 module Flit
   module Commands
     class Delete
-      DESC = "Delete a feature or bugfix branch"
+      DESC = "Delete a feature or bugfix branch. Use with caution."
       USAGE = "TYPE NAME"
       OPTIONS = {
         :type => "Can be either a feature or bugfix",
@@ -9,7 +9,18 @@ module Flit
       }
       
       def run(args)
+        # Gatekeepers
+        fatal "directory isn't a flit repository" unless is_flit?
+        show_help unless args.count == 2
         
+        type, name = args
+        config = open_config
+        
+        h = HighLine.new
+        
+        if h.agree("Delete #{type} branch '#{name}'? [yn]", true)
+          `git branch -d #{type}/#{name}`
+        end
       end
       
     end
