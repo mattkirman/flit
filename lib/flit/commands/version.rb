6f04129762ab221ba81caa45d7af68670a6897b0
Matt Kirman <matt@mattkirman.com>
2010-08-31 21:28:21 +0100

    Added the 'version' command

--- lib/flit/commands/version.rb
+++ lib/flit/commands/version.rb
@@ -1,10 +1,27 @@
 module Flit
   module Commands
     class Version
-      DESC = "Mark the current commit as a new version"
+      DESC = "Mark the current commit as a new version. Creates a new annotated tag."
+      USAGE = "VERSION [MESSAGE]"
+      OPTIONS = {
+        :version => "Should be a version string in the form X.Y.Z",
+        :message => "An optional message to add to the tag. Defaults to 'Bump to X.Y.Z'"
+      }
       
       def run(args)
+        # Gatekeepers
+        fatal "directory isn't a flit repository" unless is_flit?
+        show_help unless args.count >= 1
         
+        Hooks.fire :didStartCommand__version
+        
+        config = open_config
+        version, message = args
+        message ||= "Bump to #{config[:tags][:version_prefix]}#{version}"
+        
+        `git tag -a #{config[:tags][:version_prefix]}#{version} -m "#{message}"`
+        
+        Hooks.fire :didFinishCommand__version
       end
       
     end
